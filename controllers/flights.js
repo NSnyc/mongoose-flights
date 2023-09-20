60e43869492154ab8cabd3dc8236e7a3febe0089
Enes <enesvelovic@gmail.com>
2023-09-20 15:55:34 -0400

    Add Index Functionality

---
+++ controllers/flights.js
@@ -0,0 +1,19 @@
+import { Flight } from "../Models/flight.js";
+
+function index(req, res) {
+  Flight.find({})
+  .then(flights => {
+    res.render('flights/index', {
+      flights,
+      title: 'All Flights'
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/flights/new')
+  })
+}
+
+export {
+  index,
+}
